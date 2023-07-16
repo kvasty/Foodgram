1aceea1d18e522a7b6e2ece8755a2fc458c5e6d5
Анастасия Кульбака <super.nasta105@yandex.ru>
2023-07-16 19:42:54 +0500

    238

--- backend/api/paginators.py
+++ backend/api/paginators.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class LimitPagePagination(PageNumberPagination):
-    """Cписок первых шести рецептов"""
+    """Cписок первых шести или limit рецептов"""
     page_size_query_param = 'limit'
     page_size = 6
